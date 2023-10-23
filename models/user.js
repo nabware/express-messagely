4708656f93fb30c4e27d054c4484eed453ca6b2e
Nabeel Rana <nabwaregroup@gmail.com>
2023-10-23 11:12:46 -0700

    Added two User methods

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
 /** User of the site. */
 
 class User {
@@ -9,11 +13,41 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users (username,
+                          password,
+                          first_name,
+                          last_name,
+                          phone,
+                          join_at,
+                          last_login_at)
+         VALUES
+           ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+         RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]);
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT password
+         FROM users
+         WHERE username = $1`,
+      [username]);
+    const user = result.rows[0];
+
+    if (user) {
+      if (await bcrypt.compare(password, user.password) === true) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   /** Update last_login_at for user */
